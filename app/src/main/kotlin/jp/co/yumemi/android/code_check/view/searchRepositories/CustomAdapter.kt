d7c024ff0e9b9acd7052b9a10f240cfd5a9eabb0
sora <kacky1210@gmail.com>
2022-03-12 17:02:57 +0900

    Update: OneFragmentを整理した

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/view/searchRepositories/CustomAdapter.kt
@@ -0,0 +1,54 @@
+package jp.co.yumemi.android.code_check.view.searchRepositories
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import jp.co.yumemi.android.code_check.R
+import jp.co.yumemi.android.code_check.model.entity.item
+
+// リストの変更を検知
+private object DiffCallBack: DiffUtil.ItemCallback<item>() {
+    override fun areItemsTheSame(oldItem: item, newItem: item): Boolean
+    {
+        return oldItem.name == newItem.name
+    }
+
+    override fun areContentsTheSame(oldItem: item, newItem: item): Boolean
+    {
+        return oldItem == newItem
+    }
+}
+
+class CustomAdapter(
+    private val itemClickListener: OnItemClickListener,
+) : ListAdapter<item, CustomAdapter.ViewHolder>(DiffCallBack) {
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+    interface OnItemClickListener{
+        fun itemClick(item: item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
+    {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int)
+    {
+        val item = getItem(position)
+        holder.itemView.apply {
+            findViewById<TextView>(R.id.repositoryNameView).text = item.name
+
+            setOnClickListener {
+                itemClickListener.itemClick(item)
+            }
+        }
+    }
+}
